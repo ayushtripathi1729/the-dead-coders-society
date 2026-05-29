b65dd57d8dcb11cb13ac283c7394d6920608a546
ayushtripathi1729 <ayushtripathi2822@gmail.com>
2026-05-30 00:21:06 +0530

    debug admin login

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcryptjs");
 
-const password = "societycodersdead@1729";
+const password = "hacking is not easy";
 const hash = "$2b$10$ESZ8DpIVHEpx5fjFjP9yOeQF.ZwULei/oyxHq/l0oMJ5MEA9kqqlO";
 
 bcrypt.compare(password, hash).then(console.log);
