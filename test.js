8f1bb9eb9ee5055140df413bc60f06ba49c56344
ayushtripathi1729 <ayushtripathi2822@gmail.com>
2026-05-30 00:22:53 +0530

    security fixes

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcryptjs");
 
-const password = "hacking is not easy";
-const hash = "$2b$10$ESZ8DpIVHEpx5fjFjP9yOeQF.ZwULei/oyxHq/l0oMJ5MEA9kqqlO";
+const password = "test";
+const hash = "test";
 
 bcrypt.compare(password, hash).then(console.log);
